ee14c7b9235c5a7f3ad295c25a82260b709811cb
Artur Vincent <vartur@echiquier.educ.insa>
2013-12-06 10:59:33 +0100

    coucou

---
+++ src/main/java/fr/insa/rennes/App.java
@@ -0,0 +1,14 @@
+package fr.insa.rennes;
+
+/**
+ * Hello world!
+ * @class pui
+ * @author Morgane et Vartur
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
